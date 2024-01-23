d7e989b273e4076b8a667c9b43ba649b0bc1e110
Char_Aquivz <132508167+Naofumy585@users.noreply.github.com>
2024-01-23 13:29:30 -0600

    Version_2

--- index.php
+++ index.php
@@ -1,11 +1,81 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-MEX">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pomperrier&display=swap">
+    <link rel="stylesheet" href="src/Bootstrap/css/style_index.css">
+    <link rel="stylesheet" href="src/Bootstrap/css/bootstrap.min.css">
+    <title>Repositorio 6°J</title>
 </head>
 <body>
-    
+
+    <div>
+        <form>
+            <section>
+                <nav class="navbar navbar-expand-lg navbar-light static-top">
+                    <ul class="navbar-nav ml-auto" class="nav justify-content-end">
+                        <li class="navbar-brand"><p><b>RP </b>6</p></li>
+                        <li class="nav-item"><a class="nav-link active" href="#">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Acerca de</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                        <li class="nav-item"><a class="nav-link disable" href="#" tabindex="-1" aria-disabled="true">Contacto</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section id="Tamaños">
+                <img src="src/img/RPDG.jpg" alt="Banner" class="banner img-fluid mb-4">
+                <div class="textoBanner">Repositorio 6_J
+                    <p><b>Materia :</b>Derecho Informatico</p>
+                    <p><b>Profesor :</b>Hector Nuricumbo Castro Martinez</p>
+                    <p><b>Alumno :</b>Charly Aquino Vazquez</p>
+                </div>
+                <!-- Tabla con espacio para 4 imágenes -->
+                <div class="Img-content row">
+                    <div class="Caja col-md-4">
+                        <img src="src/img/Libro.png" alt="Producto 1" class="img-fluid">
+                        <div class="info">
+                            <p><b>Ciberseguridad</b> By <b>#</b></p>
+                        </div>
+                    </div>
+                    <div class="Caja col-md-4">
+                        <img src="src/img/Libro.png" alt="Producto 2" class="img-fluid">
+                        <div class="info">
+                            <p><b>Redes y enrutadores</b> By <b>#</b></p>
+                        </div>
+                    </div>
+                    <div class="Caja col-md-4">
+                        <img src="src/img/Libro.png" alt="Producto 3" class="img-fluid">
+                        <div class="info">
+                            <p><b>Derechos informaticos </b> By <b>Nuricumbo</b></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Sección de redes sociales e información -->
+            <section class="row">
+                <div style="background-color: #271591;" >
+                    <tr height="100px">
+                        <td>
+                            <p><b>Encuentranos</b></p>
+                            <p>#</p>
+                        </td>
+                    </tr>
+                </div>
+                <div style="background-color: #a89210;">
+                    <tr height="100px"><!--Pie de pagina-->
+                        <td>
+                            <p>&copy; <b>Derecho informatico</b> Derechos reservados</p>
+                            <h6>Creado por: <b>Charly Aquino Vazquez</b></h6>
+                        </td>
+                    </tr>
+                </div>
+            </section>
+        </form>
+    </div>
+    <!-- Agrega la referencia al script de Bootstrap al final del body -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
